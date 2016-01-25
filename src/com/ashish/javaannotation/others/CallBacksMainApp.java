58ac5e556e4ed4405ce9f007aac3a8d151e75069
Adore Ashish <adoreashish@gmail.com>
2016-01-25 13:14:03 +0530

    Adding shutdown hook to run cleanup() method

--- src/com/ashish/javaannotation/others/CallBacksMainApp.java
+++ src/com/ashish/javaannotation/others/CallBacksMainApp.java
@@ -1,16 +1,17 @@
 package com.ashish.javaannotation.others;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 public class CallBacksMainApp{
 	
 	@SuppressWarnings("resource")
 	public static void main(String args[]){
 		
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(CallBackAppConfig.class);
+		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(CallBackAppConfig.class);
 		Foo foo = (Foo) ctx.getBean(Foo.class);
 		foo.getFoo();
+		ctx.registerShutdownHook();
 		
 	}
 	
